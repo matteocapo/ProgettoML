437844230fe4dc1e89298fb1754e16595fd8ce22
Vincenzo Battisti <vincenzo.battisti@gmail.com>
2018-08-24 16:56:32 +0200

    aggiunta

--- csvLogic.py
+++ csvLogic.py
@@ -1,3 +1,48 @@
 #Logica del programma
 
-#el nino
+import pandas as pd
+import csv
+
+#lettura del dataset importato
+def readCsv(title):
+    dataset = pd.read_csv(title, sep=',')
+    return dataset
+
+#funzione per filtrare il dataset in base ai valori impostati nella gui
+def createCsv(dataset, dict_values):
+    with open('filteredcsv.csv', 'w', newline='') as csvfile:
+        newfile = csv.writer(csvfile, delimiter=',')
+        newfile.writerow(dataset.columns.values)
+        for i in range(dataset.shape[0]):
+            flag = True
+            for key,value in dict_values.items():
+                if(checkColType(dataset,key)=="int64"):
+                    if(dataset[key][i]<value):
+                        flag = False
+                elif(checkColType(dataset,key)=="object"):
+                    if(dataset[key][i]!=value):
+                        flag = False
+            if(flag):
+                newfile.writerow(dataset.iloc[i])
+
+#controllo del tipo della colonna considerata
+def checkColType(dataset, column):
+    # for key, value in dict_values.items():
+    #     if(isinstance(value, str)):
+    #         print(key + " è di tipo stringa!")
+    #     elif (isinstance(value, int)):
+    #         print(key + " è di tipo intero!")
+    return dataset[column].dtype
+
+
+# def main():
+#     title = "Family Income and Expenditure.csv"
+#     dataset = readCsv(title)
+#     dict_values = {}
+#     dict_values["Household Head Age"] = 40
+#     dict_values["Total Number of Family members"] = 3
+#     dict_values["Region"] = "CAR"
+#     #createCsv(dataset, dict_values)
+#     print(dataset["Region"].unique())
+#
+# main()
