16bf409b65d3c3bbe0c1fe5cf49d6c6ef37dfd18
jerryildrago <matteo.capodicasa@gmail.com>
2018-08-27 18:42:43 +0200

    modifica gui

--- csvGui.py
+++ csvGui.py
@@ -17,21 +17,81 @@ def main():
     def send():
         dataset = readCsv(txt0.get())
 
-        dict_values = {}
+        dict_values = dict()
         dict_values[lbl2.cget("text")] = txt1.get()
         dict_values[lbl3.cget("text")] = combo1.get()
-        dict_values[lbl4.cget("text")] = combo2.get()
-        dict_values[lbl5.cget("text")] = combo3.get()
-        dict_values[lbl6.cget("text")] = combo4.get()
-        dict_values[lbl7.cget("text")] = combo5.get()
+
+        if combo2.get() != "null":
+            dict_values[lbl4.cget("text")] = combo2.get()
+
+        marital = []
+        if chk_state2.get() == 1:
+            marital.append("Annulled")
+        if chk_state3.get() == 1:
+            marital.append("Divorced/Separated")
+        if chk_state4.get() == 1:
+            marital.append("Married")
+        if chk_state5.get() == 1:
+            marital.append("Single")
+        if chk_state6.get() == 1:
+            marital.append("Unknown")
+        if chk_state7.get() == 1:
+            marital.append("Widowed")
+
+        if marital:
+            dict_values[lbl5.cget("text")] = marital
+
+        sex = []
+        if chk_state.get() == 1 and chk_state1.get() == 1:
+            sex.append("Male")
+            sex.append("Female")
+        else:
+            if chk_state.get() == 1:
+                sex.append("Male")
+            if chk_state1.get() == 1:
+                sex.append("Female")
+        if sex:
+            dict_values[lbl6.cget("text")] = sex
+
+        main = []
+        if chk_state8.get() == 1:
+            marital.append("Enterpreneurial Activities")
+        if chk_state9.get() == 1:
+            marital.append("Other source of Income")
+        if chk_state10.get() == 1:
+            marital.append("Wage/Salaries")
+        if main:
+            dict_values[lbl7.cget("text")] = main
+
         dict_values[lbl8.cget("text")] = txt2.get()
-        dict_values[lbl9.cget("text")] = combo6.get()
+
+        if combo6.get() != "null":
+            dict_values[lbl9.cget("text")] = combo6.get()
+
         dict_values[lbl10.cget("text")] = txt3.get()
         dict_values[lbl11.cget("text")] = txt4.get()
         dict_values[lbl12.cget("text")] = txt5.get()
-        dict_values[lbl13.cget("text")] = combo7.get()
+
+        house=[]
+        if chk_state11.get() == 1:
+            marital.append("Commercial/industrial/agricultural building")
+        if chk_state12.get() == 1:
+            marital.append("Duplex")
+        if chk_state13.get() == 1:
+            marital.append("Institutional living quarter")
+        if chk_state14.get() == 1:
+            marital.append("Multi-unit residential")
+        if chk_state15.get() == 1:
+            marital.append("Other building unit (e.g. cave, boat)")
+        if chk_state16.get() == 1:
+            marital.append("Single house")
+        if house:
+            dict_values[lbl13.cget("text")] = house
+
+        print (dict_values)
 
         createCsv(dataset,dict_values)
+
         print("Il csv è stato filtrato!")
 
 
@@ -40,13 +100,13 @@ def main():
 
     window = Tk()
     window.title("Welcome to SmartADV app")
-    window.geometry('500x500')
+    window.geometry('1135x500')
 
     #importa il path del file csv
     txt0 = Entry(window,width=10)
-    txt0.grid(column=2, row=0)
+    txt0.grid(column=3, row=0)
     btn = Button(window, text="Importa il file csv", command=clicked)
-    btn.grid(column=1, row=0)
+    btn.grid(column=2, row=0)
 
     #text area
     lbl2 = Label(window, text="Household Head Age", font=("Arial", 12))
@@ -66,7 +126,7 @@ def main():
     lbl4 = Label(window, text="Household Head Highest Grade Completed", font=("Arial", 12))
     lbl4.grid(column=1, row=3)
     combo2 = Combobox(window)
-    combo2['values'] = ('Teacher Training and Education Sciences Programs',
+    combo2['values'] = ('null', 'Teacher Training and Education Sciences Programs',
      'Transport Services Programs', 'Grade 3', 'Elementary Graduate',
      'Second Year High School', 'Third Year High School',
      'Business and Administration Programs', 'First Year College',
@@ -91,29 +151,73 @@ def main():
     combo2.current(0)  # set the selected item
     combo2.grid(column=2, row=3)
 
-    #combobox
+
+    #chekbox
     lbl5 = Label(window, text="Household Head Marital Status", font=("Arial", 12))
     lbl5.grid(column=1, row=4)
-    combo3 = Combobox(window)
-    combo3['values'] = ("Annulled", "Divorced/Separated", "Married", "Single", "Unknown", "Widowed")
-    combo3.current(0)  # set the selected item
-    combo3.grid(column=2, row=4)
+    chk_state2 = BooleanVar()
+    chk_state3 = BooleanVar()
+    chk_state4 = BooleanVar()
+    chk_state5 = BooleanVar()
+    chk_state6 = BooleanVar()
+    chk_state7 = BooleanVar()
 
-    #combobox
+    chk_state2.set(False)
+    chk_state3.set(False)
+    chk_state4.set(False)
+    chk_state5.set(False)
+    chk_state6.set(False)
+    chk_state7.set(False)
+
+    chk2 = Checkbutton(window, text='Annulled', var=chk_state2)
+    chk3 = Checkbutton(window, text='Divorced/Separated', var=chk_state3)
+    chk4 = Checkbutton(window, text='Married', var=chk_state4)
+    chk5 = Checkbutton(window, text='Single', var=chk_state5)
+    chk6 = Checkbutton(window, text='Unknown', var=chk_state6)
+    chk7 = Checkbutton(window, text='Widowed', var=chk_state7)
+
+    chk2.grid(column=2,row=4)
+    chk3.grid(column=3,row=4)
+    chk4.grid(column=4, row=4)
+    chk5.grid(column=5, row=4)
+    chk6.grid(column=6, row=4)
+    chk7.grid(column=7, row=4)
+
+    #chekbox
     lbl6 = Label(window, text="Household Head Sex", font=("Arial", 12))
     lbl6.grid(column=1, row=5)
-    combo4 = Combobox(window)
-    combo4['values'] = ("Male", "Female")
-    combo4.current(0)  # set the selected item
-    combo4.grid(column=2, row=5)
+    chk_state = BooleanVar()
+    chk_state1 = BooleanVar()
 
-    #combobox
+    chk_state.set(False)
+    chk_state1.set(False)
+
+    chk = Checkbutton(window, text='Male', var=chk_state)
+    chk1 = Checkbutton(window, text='Female', var=chk_state1)
+
+    chk.grid(column=2,row=5)
+    chk1.grid(column=3,row=5)
+
+
+    #chekbox
     lbl7 = Label(window, text="Main Source of Income", font=("Arial", 12))
     lbl7.grid(column=1, row=6)
-    combo5 = Combobox(window)
-    combo5['values'] = ("Enterpreneurial Activities", "Other source of Income","Wage/Salaries")
-    combo5.current(0)  # set the selected item
-    combo5.grid(column=2, row=6)
+    chk_state8 = BooleanVar()
+    chk_state9 = BooleanVar()
+    chk_state10 = BooleanVar()
+
+    chk_state8.set(False)
+    chk_state9.set(False)
+    chk_state10.set(False)
+
+    chk8 = Checkbutton(window, text='Enterpreneurial Activities', var=chk_state8)
+    chk9 = Checkbutton(window, text='Other source of Income', var=chk_state9)
+    chk10 = Checkbutton(window, text='Wage/Salaries', var=chk_state10)
+
+    chk8.grid(column=2,row=6)
+    chk9.grid(column=3,row=6)
+    chk10.grid(column=4,row=6)
+
 
     #text area
     lbl8 = Label(window, text="Miscellaneous Goods and Services Expenditure", font=("Arial", 12))
@@ -125,7 +229,7 @@ def main():
     lbl9 = Label(window, text="Region", font=("Arial", 12))
     lbl9.grid(column=1, row=8)
     combo6 = Combobox(window)
-    combo6['values'] = ("CAR", "Caraga", "VI - Western Visayas", "V - Bicol Region", "ARMM",
+    combo6['values'] = ("null", "CAR", "Caraga", "VI - Western Visayas", "V - Bicol Region", "ARMM",
      "III - Central Luzon", "II - Cagayan Valley", "IVA - CALABARZON",
      "VII - Central Visayas", "X - Northern Mindanao", "XI - Davao Region",
      "VIII - Eastern Visayas", "I - Ilocos Region", "NCR", "IVB - MIMAROPA",
@@ -151,18 +255,44 @@ def main():
     txt5 = Entry(window,width=10)
     txt5.grid(column=2, row=11)
 
-    #combobox
+
+
+
+    #chekbox
     lbl13 = Label(window, text="Type of Building/House", font=("Arial", 12))
-    lbl13.grid(column=1, row=12)
-    combo7 = Combobox(window)
-    combo7['values'] = ("Commercial/industrial/agricultural building", "Duplex", "Institutional living quarter", "Multi-unit residential", "Other building unit (e.g. cave, boat)", "Single house")
-    combo7.current(0)  # set the selected item
-    combo7.grid(column=2, row=12)
+    lbl13.grid(column=1, row=13)
+    chk_state11 = BooleanVar()
+    chk_state12 = BooleanVar()
+    chk_state13 = BooleanVar()
+    chk_state14 = BooleanVar()
+    chk_state15 = BooleanVar()
+    chk_state16 = BooleanVar()
+
+
+    chk_state11.set(False)
+    chk_state12.set(False)
+    chk_state13.set(False)
+    chk_state14.set(False)
+    chk_state15.set(False)
+    chk_state16.set(False)
+
+    chk11 = Checkbutton(window, text='Commercial/industrial/agricultural building', var=chk_state11)
+    chk12 = Checkbutton(window, text='Duplex', var=chk_state12)
+    chk13 = Checkbutton(window, text='Institutional living quarter', var=chk_state13)
+    chk14 = Checkbutton(window, text='Multi-unit residential', var=chk_state14)
+    chk15 = Checkbutton(window, text='Other building unit (e.g. cave, boat)', var=chk_state15)
+    chk16 = Checkbutton(window, text='Single house', var=chk_state16)
+
+    chk11.grid(column=2,row=12)
+    chk12.grid(column=3,row=12)
+    chk13.grid(column=2,row=14)
+    chk14.grid(column=2,row=13)
+    chk15.grid(column=3,row=13)
+    chk16.grid(column=3,row=14)
 
     #importa il path del file csv
     btn2 = Button(window, text="Trasforma il file csv", command=send)
-    btn2.grid(column=1, row=13)
-
+    btn2.grid(column=1, row=15)
 
     window.mainloop()
 
